f69e5303c952efa222c349d02017fd9480bd4ea2
“jeffmccommas” <jeffmccommas@gmail.com>
2014-11-23 09:47:58 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require('express'),
+  stylus = require('stylus'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+if(env === 'development') {
+  mongoose.connect('mongodb://localhost/productmanager');
+} else {
+  mongoose.connect('mongodb://jmccommas:fullsail@ds059220-a0.mongolab.com:59220,ds059220-a1.mongolab.com:59220/productmanager');
+
+}
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('multivision db opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage= new Message({message: mongoMessage});
+console.log(mongoMessage.message);
+Message.findOne().exec(function(err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+  res.render('index', {
+    mongoMessage: mongoMessage
+  });
+});
+
+mongoMessage.save(function(err, doc) {
+    Message.findOne().exec(function(err, messageDoc) {
+        mongoMessage = messageDoc.message;
+    });
+});
+
+var port = process.env.PORT || 3030;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
