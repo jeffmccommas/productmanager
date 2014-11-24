1cfb95c16595d3dc0d460484bdf60ecf05f7d1fe
Shashwat Tripathi <stripathix@gmail.com>
2014-11-25 00:37:27 +0530

    ASD

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,12 @@
 angular.module('app', ['ngResource', 'ngRoute']);
 
-angular.module('app').config(function($routeProvider, $locationProvider) {
+angular.module('app').config(function ($routeProvider, $locationProvider) {
     var routeRoleChecks = {
-        admin: {auth: function(mvAuth) {
-            return mvAuth.authorizeCurrentUserForRoute('admin')
-        }}
+        admin: {
+            auth: function (mvAuth) {
+                return mvAuth.authorizeCurrentUserForRoute('admin')
+            }
+        }
     };
 
     $routeProvider
@@ -12,19 +14,23 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
             templateUrl: '/partials/main/main',
             controller: 'mvMainCtrl'
         })
-         .when('/admin/users', { templateUrl: '/partials/admin/user-list',
-            controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
+        .when('/admin/users', {
+            templateUrl: '/partials/admin/user-list',
+            controller: 'mvUserListCtrl',
+            resolve: routeRoleChecks.admin
         })
-        .when('/signup', { templateUrl: '/partials/account/signup',
+        .when('/signup', {
+            templateUrl: '/partials/account/signup',
             controller: 'mvSignupCtrl'
         })
 
 });
 
-angular.module('app').run(function($rootScope, $location) {
-    $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection) {
-        if(rejection === 'not authorized') {
+angular.module('app').run(function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function (evt, current, previous, rejection) {
+        if (rejection === 'not authorized') {
             $location.path('/');
+            console.log("HEROKU TEST");
         }
     })
 });
