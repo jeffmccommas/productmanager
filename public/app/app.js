4bdbd90145d6bbe49db32638cd85926e1fd9b2c5
“jeffmccommas” <jeffmccommas@gmail.com>
2014-11-24 15:13:04 -0500

    added base url

--- public/app/app.js
+++ public/app/app.js
@@ -10,7 +10,7 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
         }}
     };
 
-    //$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
     $routeProvider
         .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
         .when('/admin/users', { templateUrl: '/partials/admin/user-list',
