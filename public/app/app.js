0f86d85cec926dbf5f3001993c8be52a9f6d50c9
“jeffmccommas” <jeffmccommas@gmail.com>
2014-11-25 16:03:47 -0500

    rebased reop

--- public/app/app.js
+++ public/app/app.js
@@ -55,6 +55,7 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
             if (!mvIdentity.isAuthenticated()) {
                 if (next.templateUrl === "/partials/account/signup") {
 
+<<<<<<< Updated upstream
                 } else {
                     $location.path("/signup");
                 }
@@ -62,3 +63,87 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
         }
     });
 });
+=======
+});
+
+//app.config(["$stateProvider",
+//        "$urlRouterProvider",
+//        function ($stateProvider, $urlRouterProvider) {
+//            $urlRouterProvider.otherwise("/");
+//
+//            $stateProvider
+//                .state("home", {
+//                    url: "/",
+//                    templateUrl: "app/welcomeView.html"
+//                })
+//                // Products
+//                .state("productList", {
+//                    url: "/products",
+//                    templateUrl: "app/products/productListView.html",
+//                    controller: "ProductListCtrl as vm"
+//                })
+//                .state("productEdit", {
+//                    abstract: true,
+//                    url: "/products/edit/:productId",
+//                    templateUrl: "app/products/productEditView.html",
+//                    controller: "ProductEditCtrl as vm",
+//                    resolve: {
+//                        productResource: "productResource",
+//
+//                        product: function (productResource, $stateParams) {
+//                            var productId = $stateParams.productId;
+//                            return productResource.get({ productId: productId }).$promise;
+//                        }
+//                    }
+//                })
+//                .state("productEdit.info", {
+//                    url: "/info",
+//                    templateUrl: "app/products/productEditInfoView.html"
+//                })
+//                .state("productEdit.price", {
+//                    url: "/price",
+//                    templateUrl: "app/products/productEditPriceView.html"
+//                })
+//                .state("productEdit.tags", {
+//                    url: "/tags",
+//                    templateUrl: "app/products/productEditTagsView.html"
+//                })
+//                .state("productDetail", {
+//                    url: "/products/:productId",
+//                    templateUrl: "app/products/productDetailView.html",
+//                    controller: "ProductDetailCtrl as vm",
+//                    resolve: {
+//                        productResource: "productResource",
+//
+//                        product: function (productResource, $stateParams) {
+//                            var productId = $stateParams.productId;
+//                            return productResource.get({ productId: productId }).$promise;
+//                        }
+//                    }
+//                })
+//                .state("priceAnalytics", {
+//                    url: "/priceAnalytics",
+//                    templateUrl:"app/prices/priceAnalyticsView.html",
+//                    controller: "PriceAnalyticsCtrl",
+//                    resolve: {
+//                        productResource: "productResource",
+//
+//                        products: function (productResource) {
+//                            return productResource.query(function(response) {
+//                                    // no code needed for success
+//                                },
+//                                function(response) {
+//                                    if (response.status == 404) {
+//                                        alert("Error accessing resource: " +
+//                                        response.config.method + " " +response.config.url);
+//                                    } else {
+//                                        alert(response.statusText);
+//                                    }
+//                                }).$promise;
+//
+//                        }
+//                    }
+//                })
+//        }]
+//);
+>>>>>>> Stashed changes
