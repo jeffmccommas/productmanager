3a01f5a44dfacfa6332a32310dfebad928f6cdd6
Shashwat Tripathi <stripathix@gmail.com>
2014-11-27 02:35:13 +0530

    CSS for tab

--- public/app/app.js
+++ public/app/app.js
@@ -67,7 +67,6 @@ angular.module('app').config(function ($stateProvider, $locationProvider, $urlRo
         })
 }).run(function ($rootScope, $location, mvIdentity, $state) {
     $rootScope.$on('$stateChangeStart', function (event, next, toParams, current, fromParams) {
-        return;
         var requireAutantication = ["/partials/admin/products/productListView"];
         if (requireAutantication.indexOf(next.templateUrl) >= 0) {
             if (!mvIdentity.isAuthenticated()) {
