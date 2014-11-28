6472f41bada2e4deb86194f7f352733884a06d40
“jeffmccommas” <jeffmccommas@gmail.com>
2014-11-28 15:56:04 -0500

    delete modal

--- public/app/admin/products/productListCtrl.js
+++ public/app/admin/products/productListCtrl.js
@@ -1,11 +1,8 @@
-/**
- * Created by Deb on 8/20/2014.
- */
 (function () {
     "use strict";
     angular
         .module("app")
-        .controller("ProductListCtrl", ['httprequest','$scope', '$modal', ProductListCtrl]);
+        .controller("ProductListCtrl", ['httprequest', '$scope', '$modal', ProductListCtrl]);
 
     function ProductListCtrl(httprequest, $scope, $modal) {
         var vm = this;
@@ -28,22 +25,32 @@
             var to_delete_prod = vm.products[index];
             httprequest.http_delete('/api/product/' + to_delete_prod._id).then(function (response) {
                 vm.products.splice(index, 1);
-                $modalInstance.close();
                 toastr.error("Product Deleted");
             });
         };
 
+        //$scope.ok = function () {
+        //    $modalInstance.close();
+        //};
+
         $scope.cancel = function () {
-            $modalInstance.dismiss('cancel');
+            $modal.dismiss('cancel');
         };
 
-        $scope.open = function (size) {
-
+        $scope.open = function (index) {
             var modalInstance = $modal.open({
                 templateUrl: 'app/admin/products/deleteModal.html',
-                controller: 'ModalInstanceCtrl'
+                controller: function ($scope, $modalInstance) {
+                    $scope.confirmDelete = function (status) {
+                        $modalInstance.close(status);
+                    };
+                }
+            });
+            modalInstance.result.then(function (isDeleted) {
+                if (isDeleted) {
+                    vm.delete(index);
+                }
             });
         };
     }
 }());
-//// sample data for products
